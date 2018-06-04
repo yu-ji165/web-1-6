6310dfbd1b71d7c522f446ebaac3b1e3ed156c40
yu-ji <enpatsushakugan@icloud.com>
2018-06-04 18:10:25 +0900

    結局ボタン作れませんでした

---
+++ CatEscape/Assets/GameD.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameD : MonoBehaviour {
+
+    GameObject hp;
+    int playerHp;
+
+	// Use this for initialization
+	void Start () {
+        this.hp = GameObject.Find("hp");
+        playerHp = 10;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void DecHp()
+    {
+        this.hp.GetComponent<Image>().fillAmount -= 0.1f;
+        playerHp--;
+    }
+}
